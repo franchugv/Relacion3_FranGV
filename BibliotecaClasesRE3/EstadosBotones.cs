10c5353529845b5760afd36635a5fbf9ef162e5d
Francisco Baena <insfrana@outlook.com>
2024-10-16 13:41:11 +0200

    ghjg

---
+++ BibliotecaClasesRE3/EstadosBotones.cs
@@ -0,0 +1,48 @@
+﻿namespace BibliotecaClasesRE3
+{
+    // Enum
+    public enum ColoresBoton : byte { Blue, Green, Red }
+
+    public class EstadosBotones
+    {
+        // MIEMBROS
+        private ColoresBoton _coloresBoton;
+
+        // CONSTRUCTORES
+
+        public EstadosBotones()
+        {
+            _coloresBoton = ColoresBoton.Blue;
+        }
+
+        public EstadosBotones(ColoresBoton coloresBoton)
+        {
+            ColoresBotones = coloresBoton;
+        }
+
+        // PROPIEDADES
+
+        public ColoresBoton ColoresBotones
+        {
+            get
+            {
+                return _coloresBoton;
+            }
+            set
+            {
+                _coloresBoton = value;
+            }
+        }
+
+        // MÉTODOS
+
+        public void AsignacionColores()
+        {
+            if (ColoresBotones == ColoresBoton.Blue) ColoresBotones = ColoresBoton.Green;
+            else if (ColoresBotones == ColoresBoton.Green) ColoresBotones = ColoresBoton.Red;
+            else ColoresBotones = ColoresBoton.Blue;
+        }
+
+
+    }
+}
